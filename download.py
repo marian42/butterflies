000a2486db0711d05d445b53400741e9cefdfe68
Marian <mail@marian42.de>
2019-12-07 09:05:13 +0100

    Don't use the ids from the dataset

--- download.py
+++ download.py
@@ -8,8 +8,11 @@ rows = rows[1:]
 from tqdm import tqdm
 from urllib.request import Request, urlopen
 
+index = 0
+
 for row in tqdm(rows):
     request = Request(row[5])
-    image_file = open('data/raw/{:s}.jpg'.format(row[0]), 'wb')
+    image_file = open('data/raw/{:d}.jpg'.format(index), 'wb')
     image_file.write(urlopen(request).read())
     image_file.close()
+    index += 1
