5e62fc61950ffa13bf9f715223c154abc0b3994c
Marian <mail@marian42.de>
2019-12-08 16:27:54 +0100

    Update download script for the bigger dataset

--- download.py
+++ download.py
@@ -1,18 +1,39 @@
 
 import csv
-file = open('data/multimedia.csv', 'r')
-reader = csv.reader(file)
-rows = [row for row in reader]
-rows = rows[1:]
+
 
 from tqdm import tqdm
 from urllib.request import Request, urlopen
+import os
 
-index = 0
+def get_ids():
+    ids_file = open('data/ids.txt', 'r')
+    lines = ids_file.readlines()
+    ids_file.close()
+    return set(int(line.strip()) for line in lines)
 
-for row in tqdm(rows):
-    request = Request(row[5])
-    image_file = open('data/raw/{:d}.jpg'.format(index), 'wb')
+
+def get_urls(ids):
+    file = open('data/multimedia.csv', 'r')
+    for row in csv.reader(file):
+        try:
+            id = int(row[0])
+            if id in ids and 'label' not in row[2]:
+                yield row[5]
+        except:
+            pass
+
+ids = get_ids()
+urls = list(get_urls(ids))
+
+for url in tqdm(urls):
+    image_id = url.split('/')[6]
+    file_name = 'data/raw/{:s}.jpg'.format(image_id)
+    
+    if os.path.exists(file_name):
+        continue
+
+    request = Request(url)
+    image_file = open(file_name, 'wb')
     image_file.write(urlopen(request).read())
     image_file.close()
-    index += 1
