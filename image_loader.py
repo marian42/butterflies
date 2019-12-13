76317632aa1c160609f069d4e461761adc4e1ce7
Marian <mail@marian42.de>
2019-12-13 21:54:14 +0100

    Use resized images instead of resizing them during loading

--- image_loader.py
+++ image_loader.py
@@ -5,33 +5,25 @@ import numpy as np
 import glob
 from skimage import io, transform
 
-
-def load_image(file_name, resolution=128):
-    image = io.imread(file_name)
-
-    image = transform.resize(image, (resolution, resolution), preserve_range=True)    
-    image = image.transpose((2, 0, 1)).astype(np.float32) / 255
-
-    return torch.from_numpy(image)
-
 class ImageDataset(Dataset):
-    def __init__(self, resolution=128):
-        file_names = glob.glob(os.path.join('data/images/', '**.jpg'), recursive=True)
+    def __init__(self, return_hashes=False):
+        file_names = glob.glob(os.path.join('data/images_128/', '**.jpg'), recursive=True)
         self.hashes = [f.split('/')[-1][:-4] for f in file_names]
-        self.resolution = resolution
+        self.return_hashes = return_hashes        
         
     def __len__(self):
         return len(self.hashes)
 
     def __getitem__(self, index):
         hash = self.hashes[index]
-        image_file_name = 'data/images/{:s}.jpg'.format(hash)
-        image = load_image(image_file_name)
+        image_file_name = 'data/images_128/{:s}.jpg'.format(hash)
         
         image = io.imread(image_file_name)
 
-        image = transform.resize(image, (self.resolution, self.resolution), preserve_range=True)    
         image = image.transpose((2, 0, 1)).astype(np.float32) / 255
+        image = torch.from_numpy(image)
 
-        return torch.from_numpy(image)
-
+        if self.return_hashes:
+            return image, hash
+        else:
+            return image
