fb108049014a3b54562f27762aa87552111d8c4d
Marian <mail@marian42.de>
2019-12-14 11:55:07 +0100

    Add an option for resolution and margin parameters in the create_image script

--- create_images.py
+++ create_images.py
@@ -2,6 +2,7 @@ from itertools import count
 
 import torch
 from torchvision import utils
+import torch.nn.functional as F
 import random
 import glob
 from shutil import copyfile
@@ -21,20 +22,32 @@ classifier.eval()
 
 file_names = glob.glob('data/raw/**.jpg', recursive=True)
 
-for file_name in tqdm(file_names):
-    hash = file_name.split('/')[-1][:-4]
-    result_file_name = 'data/images/{:s}.jpg'.format(hash)
-    if os.path.exists(result_file_name):
-        continue
+OUTPUT_RESOLUTION = None
+MARGIN = 0.1
 
-    try:
-        image = load_image(file_name).to(device)
-    except ValueError:
-        print("Could not open {:s}.".format(file_name))
-        continue
-    image = classifier.apply(image)
-    if image is None:
-        print("Found nothing in {:s}.".format(hash))
-        continue
-    
-    utils.save_image(image, result_file_name)
+with torch.no_grad():
+    for file_name in tqdm(file_names):
+        hash = file_name.split('/')[-1][:-4]
+        
+        result_file_name = 'data/images' \
+            + '' if OUTPUT_RESOLUTION is None else '_{:d}'.format(OUTPUT_RESOLUTION) \
+            + '/{:s}.jpg'.format(hash)
+        
+        if os.path.exists(result_file_name):
+            continue
+
+        try:
+            image = load_image(file_name).to(device)
+        except ValueError:
+            print("Could not open {:s}.".format(file_name))
+            continue
+        image = classifier.apply(image, margin=MARGIN)
+        
+        if image is None:
+            print("Found nothing in {:s}.".format(hash))
+            continue
+        
+        if OUTPUT_RESOLUTION is not None:
+            image = (F.adaptive_avg_pool2d(image, (OUTPUT_RESOLUTION, OUTPUT_RESOLUTION))).data
+            
+        utils.save_image(image, result_file_name)
