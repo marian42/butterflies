ee37986481ffb70c5cee5cf5f80a8bcd885c754b
Marian <mail@marian42.de>
2019-12-16 23:18:50 +0100

    Use a faster implementation of t-SNE

--- create_tsne.py
+++ create_tsne.py
@@ -1,7 +1,8 @@
 import numpy as np
-from sklearn.manifold import TSNE
+from MulticoreTSNE import MulticoreTSNE as TSNE
 
 latent_codes = np.load('data/latent_codes.npy')
-tsne = TSNE(n_components=2)
+
+tsne = TSNE(n_jobs=4)
 embedded = tsne.fit_transform(latent_codes)
 np.save('data/latent_codes_embedded.npy', embedded)
