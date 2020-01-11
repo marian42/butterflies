ea493cb5cc0f75c894ff68abf8d51ff74387f277
Marian <mail@marian42.de>
2020-01-11 15:52:10 +0100

    Read the number of CPUs for the process count of t-SNE

--- create_tsne.py
+++ create_tsne.py
@@ -1,8 +1,9 @@
 import numpy as np
+import os
 from MulticoreTSNE import MulticoreTSNE as TSNE
 
 latent_codes = np.load('data/latent_codes.npy')
 
-tsne = TSNE(n_jobs=4)
+tsne = TSNE(n_jobs=os.cpu_count())
 embedded = tsne.fit_transform(latent_codes)
 np.save('data/latent_codes_embedded.npy', embedded)
