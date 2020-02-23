ee6b653bbab3c49a6c3fa8d894c0669e3ef6d5ec
Marian <mail@marian42.de>
2020-02-23 18:37:12 +0100

    Make Verlet point relaxation script multithreaded

--- move_points_verlet.py
+++ move_points_verlet.py
@@ -1,67 +1,32 @@
 import numpy as np
 from tqdm import tqdm
 from sklearn.neighbors import KDTree
+from scipy.spatial import Voronoi
 from config import *
 from multiprocessing import Pool
 import os
 import traceback
 import time
 from matplotlib import pyplot as plt
+from matplotlib import collections  as mc
 from sklearn.cluster import DBSCAN
-from matplotlib.collections import EllipseCollection
+from scipy.spatial import ConvexHull
+import math
 
 RADIUS = IMAGE_SIZE / 2 / TILE_SIZE / 2**TILE_DEPTH
-
-plt.figure(num=None, figsize=(14, 14), dpi=140, facecolor='w', edgecolor='k')  
-
-def plot(points, index):
-    axes = plt.gca()
-    x = points[:, 0]
-    y = points[:, 1]
-    axes.set_aspect('equal', adjustable='box')
-
-    margin = 0.005
-    
-    plt.xlim((x_range[0] - margin, x_range[1] + margin))
-    plt.ylim((y_range[0] - margin, y_range[1] + margin))
-
-    distances, indices = KDTree(points).query(points, k=2, return_distance=True)
-    violating_points = distances[:, 1] < 2 * RADIUS * 0.99
-
-    size = RADIUS * 2
-    
-    axes.add_collection(EllipseCollection(widths=size, heights=size, angles=0, units='xy', offsets=points[~violating_points, :], transOffset=axes.transData, facecolor='None', edgecolor='gray'))
-    axes.add_collection(EllipseCollection(widths=size, heights=size, angles=0, units='xy', offsets=points[violating_points, :], transOffset=axes.transData, facecolor='None', edgecolor='red'))
-    
-    plt.savefig('images/{:05d}.png'.format(index), bbox_inches='tight')
-    plt.clf()
-
-
-points = np.load('data/latent_codes_embedded.npy')
-min_value = np.min(points, axis=0)
-max_value = np.max(points, axis=0)
-points -= (max_value + min_value) / 2
-points /= np.max(points, axis=0)
-
-#x_range = (-0.54, -0.44)
-#y_range = (0.14, 0.225)
-#mask = (points[:, 0] > x_range[0]) & (points[:, 0] < x_range[1]) & (points[:, 1] > y_range[0]) & (points[:, 1] < y_range[1])
-#points = points[mask, :]
 
 VELOCITY_DECAY = 0.8
 REPELL_FORCE_STRENGTH = 0.5
 RESET_FORCE_STRENGTH = 0.2 * RADIUS
 
-def move_points(points, max_iter=400):
+def move_points(points, verbose=False, max_iter=1000):
     velocity = np.zeros(points.shape)
     original_points = np.array(points)
 
     is_finalizing_phase = False
     finalizing_steps_left = 40
 
-    for step in range(max_iter):
-        #plot(points, step)
-
+    for step in tqdm(range(max_iter)) if verbose else range(max_iter):
         next_points = points + velocity
 
         distances, indices = KDTree(next_points).query(next_points, k=5, return_distance=True)
@@ -93,6 +58,8 @@ def move_points(points, max_iter=400):
 
         if not is_finalizing_phase and step % 10 == 0:
             distances, _ = KDTree(points).query(points, k=2, return_distance=True)
+            if verbose:
+                tqdm.write('Violating points: {:d}'.format(np.count_nonzero(distances[:, 1] < 2 * RADIUS)))
             if np.count_nonzero(distances[:, 1] < 2 * RADIUS * 0.85) < points.shape[0] * 0.01:
                 is_finalizing_phase = True
 
@@ -101,5 +68,86 @@ def move_points(points, max_iter=400):
             if finalizing_steps_left == 0:
                 break
 
+def get_violating_range(points, range=8):
+    distances, _ = KDTree(points).query(points, k=2, return_distance=True)
+    distances = distances[:, 1]
+    mask = distances < RADIUS * 2
 
-move_points(points)
+    if range == 0:
+        return mask
+
+    violating_points = points[mask, :]
+
+    distances, _ = KDTree(violating_points).query(points, k=1, return_distance=True)
+    return distances[:, 0] < RADIUS * 2 * range
+
+def process_cluster(points, cluster_label):
+    try:
+        move_points(points)
+        return points, cluster_label
+    except:
+        traceback.print_exc()
+    return points, cluster_label
+
+if __name__ == '__main__':
+    points = np.load('data/latent_codes_embedded.npy')
+    min_value = np.min(points, axis=0)
+    max_value = np.max(points, axis=0)
+    points -= (max_value + min_value) / 2
+    points /= np.max(points, axis=0)
+
+    move_points(points, verbose=True, max_iter=40)
+
+    parallel_working_mask = get_violating_range(points, range=8)
+    parallel_working_set = points[parallel_working_mask, :]
+
+    dbscan = DBSCAN(eps=RADIUS * 16, min_samples=1).fit(parallel_working_set)
+    cluster_labels = dbscan.labels_
+    label_count = np.max(dbscan.labels_)
+    print('Cluster count:', label_count)
+    
+    cluster_masks = dict()
+    remainder = []
+    for cluster_label in range(label_count):
+        mask = np.nonzero(cluster_labels == cluster_label)[0]
+        if mask.shape[0] >= 20:
+            cluster_masks[cluster_label] = mask
+        else:
+            remainder.append(mask)
+    cluster_masks[-1] = np.concatenate(remainder)
+    
+    worker_count = os.cpu_count()
+    pool = Pool(worker_count)
+    cluster_labels = sorted(cluster_masks.keys(), key=lambda label: cluster_masks[label].shape[0], reverse=True)
+
+    print('The largest cluster contains {:d} points (should be < 100k)'.format(cluster_masks[cluster_labels[0]].shape[0]))
+    print("Using {:d} processes.".format(worker_count))
+    
+    progress = tqdm(total=sum(cluster_masks[label].shape[0] for label in cluster_masks.keys()))
+    
+    def on_complete(args):
+        moved_points, cluster_label = args
+        mask = cluster_masks[cluster_label]
+        parallel_working_set[mask] = moved_points
+        progress.update(mask.shape[0])
+
+    for cluster_label in cluster_labels:
+        mask = cluster_masks[cluster_label]
+        current_cluster_points = np.array(parallel_working_set[mask])
+        pool.apply_async(process_cluster, args=(current_cluster_points, cluster_label), callback=on_complete)
+
+    pool.close()
+    pool.join()
+
+    points[parallel_working_mask] = parallel_working_set
+
+    move_points(points, verbose=True, max_iter=21)
+    
+    OUTPUT_FILENAME = 'data/latent_codes_embedded_moved.npy'
+    min_value = np.min(points, axis=0)
+    max_value = np.max(points, axis=0)
+    points -= (max_value + min_value) / 2
+    points *= 0.99 / np.max(points, axis=0)
+
+    np.save(OUTPUT_FILENAME, points)
+    print("Saved to {:s}.".format(OUTPUT_FILENAME))
