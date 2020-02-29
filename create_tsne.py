c3b81aca4ec5d0bfe7eb80dae743b21d80e22180
Marian <mail@marian42.de>
2020-02-29 22:43:07 +0100

    Add an option to choose between t-SNE implementations

--- create_tsne.py
+++ create_tsne.py
@@ -1,9 +1,20 @@
 import numpy as np
-import os
-from MulticoreTSNE import MulticoreTSNE as TSNE
+
+METHOD = 2
 
 latent_codes = np.load('data/latent_codes.npy')
 
-tsne = TSNE(n_jobs=os.cpu_count())
-embedded = tsne.fit_transform(latent_codes)
+if METHOD == 1: 
+    # multicore tsne
+    # https://github.com/DmitryUlyanov/Multicore-TSNE
+    from MulticoreTSNE import MulticoreTSNE as TSNE
+    import os
+    tsne = TSNE(n_jobs=os.cpu_count())
+    embedded = tsne.fit_transform(latent_codes)
+elif METHOD == 2:
+    # opentsne, uses a single core
+    # https://github.com/pavlin-policar/openTSNE
+    from openTSNE import TSNE
+    embedded = TSNE().fit(latent_codes)
+
 np.save('data/latent_codes_embedded.npy', embedded)
