245c79b7cdd2efcb815c0c96f5c02afefd8dcb80
Marian <mail@marian42.de>
2020-03-03 20:32:03 +0100

    Create a latent space interpolation in the autoencoder test script

--- test_autoencoder.py
+++ test_autoencoder.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader
 from torchvision import utils
 from tqdm import tqdm
 
-from autoencoder import Autoencoder
+from autoencoder import Autoencoder, LATENT_CODE_SIZE
 from config import *
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -21,15 +21,21 @@ autoencoder = Autoencoder(is_variational=USE_VARIATIONAL_AUTOENCODER)
 autoencoder.load_state_dict(torch.load(AUTOENCODER_FILENAME))
 autoencoder.eval()
 
+STEPS = 5
+
 with torch.no_grad():
     for sample in tqdm(data_loader):
         image, hash = sample
         hash = hash[0]
 
-        output = autoencoder.decode(autoencoder.encode(image.to(device).unsqueeze(0)))
+        latent_code = autoencoder.encode(image.to(device)).unsqueeze(0)
+
         
-        result = torch.zeros((3, 128, 256))
+        result = torch.zeros((3, 128, 128 * (STEPS + 1)))
         result[:, :, :128] = image.cpu()
-        result[:, :, 128:] = output
+
+        for i in range(STEPS):
+            output = autoencoder.decode(latent_code * (1.0 - i / (STEPS - 1)))
+            result[:, :, 128 * (i + 1):128 * (i + 2)] = output
 
         utils.save_image(result, 'data/test/{:s}.jpg'.format(hash))
