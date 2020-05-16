cfee9fe033c559e4deaeced91220a4e71575f2d8
Marian <mail@marian42.de>
2020-05-16 20:04:31 +0200

    Update scale_rotation_dataset.py to work with webp images

--- scale_rotation_dataset.py
+++ scale_rotation_dataset.py
@@ -2,23 +2,24 @@ import os
 import numpy as np
 from skimage import io, transform
 from tqdm import tqdm
+import webp
 
 OUTPUT_RESOLUTION = 128
 
 from rotation_dataset import RotationDataset
 dataset = RotationDataset()
 
-file_names = ['data/images_alpha/{:s}.png'.format(id) for id in dataset.image_ids]
-
+file_names = ['data/images_alpha/{:s}.webp'.format(id) for id in dataset.image_ids]
 
 for file_name in tqdm(file_names):    
-    hash = file_name.split('/')[-1][:-4]
+    hash = file_name.split('/')[-1][:-5]
     out_file_name = 'data/images_{:d}/{:s}.jpg'.format(OUTPUT_RESOLUTION, hash)
     if os.path.exists(out_file_name):
         continue
     try:
-        image = io.imread(file_name)
-        image = image[:, :, :3]
+        image = webp.imread(file_name).astype(np.float32)
+        alpha_mask = image[:, :, 3][:, :, np.newaxis]
+        image = image[:, :, :3] * alpha_mask / 255 + (255.0 - alpha_mask)
 
         image = transform.resize(image, (OUTPUT_RESOLUTION, OUTPUT_RESOLUTION), preserve_range=True).astype(np.uint8)
         io.imsave(out_file_name, image)
